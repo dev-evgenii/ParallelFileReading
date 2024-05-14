e18d36991e870c54b119fb0c5bb6ddb369e558ca
dev-evgenii <158760728+dev-evgenii@users.noreply.github.com>
2024-05-14 12:26:40 +0300

    Add project files.

---
+++ ParallelFileReading/Program.cs
@@ -0,0 +1,10 @@
+﻿using ParallelFileReading;
+
+// Прочитать 3 файла параллельно и вычислить количество пробелов в них (через Task).
+TaskFilesRead.ThreePredifinedFilesProcess();
+
+// Параллельное считывание файлов из папки по указанному пути и подсчёт пробелов
+Console.Write("\nЗадайте путь к папке для чтения файлы: ");
+var path = Console.ReadLine();
+
+TaskFilesRead.ParallelReadFromFolder(path ?? string.Empty);
---
+++ ParallelFileReading/TaskFilesRead.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+
+namespace ParallelFileReading;
+
+public static class TaskFilesRead
+{   
+    public static void ThreePredifinedFilesProcess()
+    {        
+        var fileDirectory = Directory.GetCurrentDirectory().Split("\\bin");
+        if (fileDirectory.Length > 1)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            string filePath1 = $"{fileDirectory[0]}\\Files\\File1.txt";
+            string filePath2 = $"{fileDirectory[0]}\\Files\\File2.txt";
+            string filePath3 = $"{fileDirectory[0]}\\Files\\File3.txt";
+
+            var task1 = FileTaskExecute(filePath1);
+            var task2 = FileTaskExecute(filePath2);
+            var task3 = FileTaskExecute(filePath3);
+
+            Task.WaitAll(task1, task2, task3);
+
+            stopwatch.Stop();
+            Console.WriteLine($"Файлы обработаны за: {stopwatch.ElapsedMilliseconds} мс");
+        }
+        else
+        {
+            Console.WriteLine("Файлы для теста метода ThreePredifinedFilesProcess не найдены !");
+        }
+    }
+
+    public static void ParallelReadFromFolder(string path)
+    {
+        if (string.IsNullOrWhiteSpace(path) || !Directory.Exists(path))
+        {
+            Console.WriteLine("Путь к папке не найден!");
+            return;
+        }
+
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        var taskList = new List<Task>();
+        var filesAll = Directory.GetFiles(path);
+        foreach (var file in filesAll)
+        {
+            taskList.Add(FileTaskExecute(file));
+        }
+
+        Task.WaitAll([.. taskList]);
+
+        stopwatch.Stop();
+        Console.WriteLine($"Файлы из папки '{path}' обработаны за: {stopwatch.ElapsedMilliseconds} мс");
+    }
+
+    private static Task FileTaskExecute(string filePath)
+    {     
+       return Task.Run(() => FileHelper.GetSpaces(filePath));
+    }
+}
