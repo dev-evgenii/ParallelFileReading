f547a8a5a4af5efc3ffab86ed0ccbcad774c1a60
dev-evgenii <developer.evgenii@yandex.ru>
2024-05-15 22:33:53 +0300

    Improve FileHelper.

--- ParallelFileReading/FileHelper.cs
+++ ParallelFileReading/FileHelper.cs
@@ -4,10 +4,8 @@ public static class FileHelper
 {
     public static void GetSpaces(string filePath)
     {
-        string text = File.ReadAllText(filePath);
-        char[] chars = text.ToCharArray();
-
-        int spaceCount = chars.Count(char.IsWhiteSpace);
+        string text = File.ReadAllText(filePath);       
+        int spaceCount = text.Count(char.IsWhiteSpace);
 
         Console.WriteLine($"В файле '{filePath}' количество пробелов: {spaceCount}");
     }
